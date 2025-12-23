066971127176cca07e147623027f1fd678d7754e
vivaan81 <vivaanoff@gmail.com>
2025-12-24 00:37:27 +0530

    task 2

--- OOPS.py
+++ OOPS.py
@@ -2,17 +2,23 @@ class Car:
     def __init__(self, make, model, price):
         self.make = make
         self.model = model
-        self.price = price
+        self.__price = price
 
     def apply_discount(self, discount):
-        self.price = self.price - (self.price * discount / 100)
+        self.__price = self.__price - (self.__price * discount / 100)
 
     def get_info(self):
         print("Make:", self.make)
         print("Model:", self.model)
-        print("Price:", self.price)
+        print("Price:", self.__price)
+
+    def get_price(self):
+    return self.__price
+
+    def set_price(self, price):
+    self.__price = price
+    
 
 
 car1 = Car("Toyota", "Corolla", 1000000)
 car1.get_info()
-#i have now completed task 1 and will now commit the code once here
